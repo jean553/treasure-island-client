1f246a181c9728463b73ad9ff4b74d2ce713b370
jean553 <lelievrejean82@gmail.com>
2021-04-12 19:55:06 +0200

    receive a message with different actions and apply those actions

--- treasure-island-client/src/threads.rs
+++ treasure-island-client/src/threads.rs
@@ -26,36 +26,38 @@ pub fn receive_message_from_stream(
 
     let mut buffer = BufReader::new(stream);
 
-    const BUFFER_LENGTH: usize = 400;
+    /* maximum data received length is 400 bytes long (for the map);
+       + 1 byte for the action */
+    const BUFFER_LENGTH: usize = 401;
     let mut message: [u8; BUFFER_LENGTH] = [0; BUFFER_LENGTH];
 
     println!("Listening for messages from the server...");
 
     loop {
 
-        /* blocking until the client receives a message from the server;
-           a line break is expected at the end of the line */
+        /* blocking */
         let _ = buffer.read(&mut message).unwrap();
 
-        /* only considers received messages if the first byte is not equal to 0 */
-        if message[0] == 0 {
+        let message_action = message[0];
+
+        const MESSAGE_ACTION_IGNORED: u8 = 0;
+        if message_action == MESSAGE_ACTION_IGNORED {
             continue;
         }
 
-        /* FIXME: we start the game immediatly when we receive the first message
-           from the server; for now, we update that value for every received message;
-           we should update it only for some type of "StartGame" message */
+        const MESSAGE_ACTION_PUSH_MAP: u8 = 1;
+        if message_action == MESSAGE_ACTION_PUSH_MAP {
+
+            let mut tiles_mutex_guard = tiles_mutex_arc.lock().unwrap();
+            let tiles = &mut *tiles_mutex_guard;
+            tiles.copy_from_slice(&message[1..BUFFER_LENGTH]);
+
+            continue;
+        }
+
         let mut waiting_for_players_mutex_guard = waiting_for_players_mutex_arc.lock().unwrap();
         let waiting_for_players = &mut *waiting_for_players_mutex_guard;
         *waiting_for_players = false;
-
-        /* block until there is no use of the tiles mutex pointer anymore */
-        let mut tiles_mutex_guard = tiles_mutex_arc.lock().unwrap();
-        let tiles = &mut *tiles_mutex_guard;
-        tiles.copy_from_slice(&message);
-
-        /* TODO: use mpsc channels to use message from the main thread */
-        println!("Received message: {:?}", message);
     }
 }
 
